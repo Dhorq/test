f5661cbf97de83f80dccd7388d38538569377117
Dhorq <ridhorifqy34@gmail.com>
2026-03-09 12:30:45 +0700

    update

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/Dhorq/test
+module github.com/Dhorq/test/v2
 
 go 1.25.5
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package test
 
-func SayHello() string {
-	return "Hello World"
+func SayHello(name string) string {
+	return "Hello " + name
 }
